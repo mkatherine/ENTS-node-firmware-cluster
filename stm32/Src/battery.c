e408f58216632ce3db07c63f5b27186108652502
John Madden <jmadden173@pm.me>
2023-11-16 13:06:27 -0800

    Template battery library

---
+++ stm32/Src/battery.c
@@ -0,0 +1,9 @@
+/**
+ * @file battery.c 
+ * @brief Provides implementations for battery monitoring interface.
+ * 
+ * @author John Madden <jmadden173@pm.me>
+ * @date 2023-11-16
+*/
+
+#include "battery.h"

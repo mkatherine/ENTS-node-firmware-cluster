69a8967485b3dbd84278a6375aab1bebf5ccd349
John Madden <jmadden173@pm.me>
2023-11-30 21:24:21 -0800

    Added example_dirtviz.cpp

---
+++ esp32/src/examples/example_dirtviz.cpp
@@ -0,0 +1,69 @@
+/**
+ * @brief Example of using dirtviz library
+ * 
+ * @author John Madden <jmadden173@pm.me>
+ * @date 2023-11-30
+ */
+
+#include <Arduino.h>
+#include <WiFi.h>
+
+#include "dirtviz.hpp"
+
+/** Baud rate for serial interface */
+#define SERIAL_BAUD 115200
+
+const char ssid[] = "my-ssid";
+const char pass[] = "pass";
+
+Dirtviz api("dirtviz.jlab.ucsc.edu", 443);
+
+const char data[] = "hello world";
+const size_t data_len = 12;
+
+/**
+ * @brief Initialization code run on startup
+ * 
+ * 
+*/
+void setup()
+{
+  // Start serial interface
+  Serial.begin(115200);
+  // Wait for serial connection
+  while (!Serial) { delay(100); }
+
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+  Serial.print("Password: ");
+  Serial.println(pass);
+  Serial.print("Connecting");
+
+  // Connect to WiFi network
+  WiFi.begin(ssid, pass);
+
+  // Wait for WiFi to connect
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Connected!");
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+  // Buffer to store response
+  static uint8_t resp[256];
+  static size_t resp_len;
+
+  // Send example measurement
+  resp_len = api.SendMeasurement((const uint8_t*) data, data_len, resp);
+
+  // Print response
+  Serial.print("Response:");
+  Serial.println((char*) resp);
+}

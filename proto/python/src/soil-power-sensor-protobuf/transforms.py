64cb09716feb32454872dbe0ab3aa9dbfce0ae04
John Madden <jmadden173@pm.me>
2024-01-02 13:47:08 -0800

    Implemented encode

--- proto/python/src/soil-power-sensor-protobuf/transforms.py
+++ proto/python/src/soil-power-sensor-protobuf/transforms.py
@@ -1,18 +1,38 @@
 """Binary data transforms
 
-@author John Madden <jmadden173@pm.me>
-@date 2024-01-01
+.. author:: John Madden <jmadden173@pm.me>
+   :date: 2024-01-01
 """
 
+from .soil_power_sensor_pb2 import Response, ResponseCode
 
-from . import soil_power_sensor_pb2
+def encode(success: bool = True):
+    """Encodes a response
+    
+    The response indicates there was a successful upload, otherwise indicates an
+    error.
+    
+    Args:
+        success: Specifies whether to encode a success or failure.
+        
+    Returns:
+        Serialized response message
+    
+    """
+   
+    # format response 
+    response = Response()
+    if success:
+        response.resp = ResponseCode.SUCCESS
+    else:
+        response.resp = ResponseCode.ERROR
+       
+    # return encode message 
+    return response.SerializeToString()
 
 
-def encode():
-    """Encode the response"""
-    pass
-
-
-def decode():
-    """Decodes the measurement data"""
-    pass
+def decode(data: bytes):
+    """Decodes the measurement data
+    """
+    
+    

564ffc893a960c1350997d87cc46af278a3d4ec8
John Madden <jmadden173@pm.me>
2024-01-02 14:34:23 -0800

    Implemented decode

--- proto/python/src/soil-power-sensor-protobuf/transforms.py
+++ proto/python/src/soil-power-sensor-protobuf/transforms.py
@@ -4,6 +4,8 @@
    :date: 2024-01-01
 """
 
+from google.protobuf.json_format import MessageToDict
+
 from .soil_power_sensor_pb2 import Measurement, Response
 
 def encode(success: bool = True):
@@ -31,8 +33,40 @@ def encode(success: bool = True):
     return response.SerializeToString()
 
 
-def decode(data: bytes):
+def decode(data: str):
     """Decodes the measurement data
+    
+    Uses protobuf to decode the serialized data and transform the measurement
+    into a dictionary.
+    
+    Args:
+        data: String of protobuf serialized data from the Measurement message
+    
+    Returns:
+        Flat dictionary of values from the meta field, measurement field, and
+        the key "type" to indicate the type of measurement
+        
+    Raises:
+        KeyError: When the serialized data is missing a requires field
     """
+
+    # parse data
+    meas = Measurement()
+    meas.ParseFromString(data)
     
+    # convert meta into dict
+    if not meas.HasField("meta"):
+        raise KeyError("Measurement missing metadata")
+    meta_dict = MessageToDict(meas.meta)
     
+    # decode measurement
+    if not meas.HasField("measurement"):
+        raise KeyError("Measurement missing data")
+    measurement_type = meas.WhichOneOf("measurement")
+    measurement_dict = MessageToDict(getattr(meas, measurement_type))
+    measurement_dict["type"] = measurement_type
+
+    # merge dictionaries
+    meas_dict = {**meta_dict, **measurement_dict}
+    
+    return meas_dict 

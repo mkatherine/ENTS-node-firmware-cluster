c44a7aabe3bf0c906b8d19461948da4a2bfd5879
John Madden <jmadden173@pm.me>
2024-02-04 14:50:53 -0800

    Updated CLK to 48MHz

--- stm32/Inc/stm32wlxx_it.h
+++ stm32/Inc/stm32wlxx_it.h
@@ -60,6 +60,7 @@ void DMA1_Channel1_IRQHandler(void);
 void DMA1_Channel2_IRQHandler(void);
 void USART1_IRQHandler(void);
 void RTC_Alarm_IRQHandler(void);
+void SUBGHZ_Radio_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
--- stm32/Src/rtc.c
+++ stm32/Src/rtc.c
@@ -96,7 +96,7 @@ void HAL_RTC_MspInit(RTC_HandleTypeDef* rtcHandle)
   /** Initializes the peripherals clocks
   */
     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
-    PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
+    PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
 
     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
     {

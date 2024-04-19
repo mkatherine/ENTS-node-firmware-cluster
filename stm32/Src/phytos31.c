9dc994a159da56e023b181b2d7a39dfed4b4a7f5
Steve <sgtaylor@ucsc.edu>
2024-04-19 10:20:34 -0700

    Merged from main into phytos

--- stm32/Src/phytos31.c
+++ stm32/Src/phytos31.c
@@ -15,6 +15,7 @@
 #include <string.h>
 
 #include "phytos31.h"
+#include "transcoder.h"
 
 HAL_StatusTypeDef Phytos31Init(){
   return ADS12_init();
@@ -22,5 +23,23 @@ HAL_StatusTypeDef Phytos31Init(){
 
 phytos_measurments Phytos31GetMeasurment(){
   phytos_measurments measurments;
-  
+  measurments.phytos31_raw = (double) ADC_readVoltage();
+  return measurments;
+}
+
+size_t Phytos31_measure(uint8_t *data) {
+  // get timestamp
+  time_t ts = TimestampNow();
+  phytos_measurments measurment;
+
+  // read voltage
+  measurment = Phytos31GetMeasurment();
+  double adc_voltage_float = ((double) measurment.phytos31_raw) / 1000.;
+
+  // encode measurement
+  size_t data_len = EncodePhytos31Measurement((uint32_t) ts, LOGGER_ID, CELL_ID,
+                                           adc_voltage_float, 0.0, data);
+
+  // return number of bytes in serialized measurement 
+  return data_len;
 }

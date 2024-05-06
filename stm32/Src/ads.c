d0cc5615b453f7b238f6054c39723f0056b12540
Steve <sgtaylor@ucsc.edu>
2024-05-06 16:51:00 -0700

    calibration changes for john

--- stm32/Src/ads.c
+++ stm32/Src/ads.c
@@ -158,8 +158,11 @@ size_t ADC_measure(uint8_t *data) {
   SysTime_t ts = SysTimeGet();
 
   // read voltage
-  double adc_voltage = ADC_readVoltage();
-  double adc_current = ADC_readCurrent();
+  // double adc_voltage = ADC_readVoltage();
+  // double adc_current = ADC_readCurrent();
+    double adc_voltage = 50.0;
+  double adc_current = 25.0;
+  ts.Seconds = 100;
 
   // encode measurement
   size_t data_len = EncodePowerMeasurement(ts.Seconds, LOGGER_ID, CELL_ID,

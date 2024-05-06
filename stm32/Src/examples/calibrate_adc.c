1ab88e1fad8b21398896344013d07087732dabb7
Steve <sgtaylor@ucsc.edu>
2024-05-06 14:09:41 -0700

    sps side tx sends meas size and encoded meas

--- stm32/Src/examples/calibrate_adc.c
+++ stm32/Src/examples/calibrate_adc.c
@@ -125,7 +125,7 @@ int main(void)
   char controller_input[3];
   char check_input[7];
   char check_result[4];
-  char size_proto_string[250];
+  char size_proto_string[4];
   char encoded_measurment[256];
   int size_check = sprintf(check_result, "ok\n");
   bool checked = false;
@@ -151,10 +151,10 @@ int main(void)
 
     HAL_UART_Receive(&huart1, (uint8_t *) controller_input, 1, 1000); // On every other iteration, send the encoded measurment in response to the '0' command
     if (controller_input[0] == '0'){
-      size_t measure = ADC_measure(&encoded_measurment);
-      size_check = sprintf(size_proto_string, "%d\n", measure);
-      HAL_UART_Transmit(&huart1, (uint8_t *) size_proto_string,size_check, 100);
-      HAL_UART_Transmit(&huart1, (uint8_t *) encoded_measurment, measure, 100);
+      size_t measurement_size = ADC_measure(&encoded_measurment); // Read the measurment, and store it's size in measurement_size (size int 64)
+      snprintf(size_proto_string, sizeof(size_proto_string), "%03d", measurement_size); // Turn measurement_size into a string of the max possible bytes, with leading 0s if neccesarry
+      HAL_UART_Transmit(&huart1, (uint8_t *) size_proto_string, strlen(size_proto_string), 100); // Send all 64 bytes
+      HAL_UART_Transmit(&huart1, (uint8_t *) encoded_measurment, measurement_size, 100);
     } 
     //HAL_Delay(100);
   /* USER CODE END 3 */

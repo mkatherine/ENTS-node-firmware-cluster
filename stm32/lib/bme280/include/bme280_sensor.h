29f34216c2acf408123b01cece13c97a1c3c4e6a
John Madden <jmadden173@pm.me>
2024-05-17 14:12:44 -0700

    Added init and measurement functions

--- stm32/lib/bme280/include/bme280_sensor.h
+++ stm32/lib/bme280/include/bme280_sensor.h
@@ -24,24 +24,49 @@
 #include "stm32wlxx_hal_i2c.h"
 #include "stm32wlxx_hal_def.h"
 
+#include "bme280.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/**
+ * @brief Struct to store data from sensor
+ * 
+ */
+typedef struct bme280_data BME280Data;
+
+/**
+ * @brief BME280 error codes
+ * 
+ * The enum constants are defined to match error codes in bme280_defs.h
+ */
 typedef enum {
-  BME280_OK,
-  BME280_ERROR
+  BME280_STATUS_OK = BME280_OK,
+  BME280_STATUS_NULL_PTR = BME280_E_NULL_PTR,
+  BME280_STATUS_COMM_FAIL = BME280_E_COMM_FAIL,
+  BME280_STATUS_INVALID_LEN = BME280_E_INVALID_LEN,
+  BME280_STATUS_DEV_NOT_FOUND = BME280_E_DEV_NOT_FOUND,
+  BME280_STATUS_SLEEP_MODE_FAIL = BME280_E_SLEEP_MODE_FAIL,
+  BME280_STATUS_NVM_COPY_FAILED = BME280_E_NVM_COPY_FAILED,
+  BME280_STATUS_ERROR = -7
 } BME280Status;
 
 BME280Status BME280Init(void);
 
 BME280Status BME280Deinit(void);
 
-BME280Status BME280MeasureTemperature(void);
-
-BME280Status BME280MeasurePressure(void);
-
-BME280Status BME280MeasureHumidity(void);
+/**
+ * @brief Measures temperature, pressure, and humidity
+ * 
+ * If you only need a single measurement channel, take all measurements and only
+ * take the needed measurement. Internally all channels need to be measured to
+ * get the value of a single channel.
+ * 
+ * @param data 
+ * @return Status code
+ */
+BME280Status BME280MeasureAll(BME280Data *data);
 
 /**
  * @brief BME280 sensor library function
--- stm32/lib/bme280/src/bme280_sensor.c
+++ stm32/lib/bme280/src/bme280_sensor.c
@@ -1,11 +1,29 @@
 #include "bme280_sensor.h"
 
-BME280Status BME280Init(uint16_t addr)
-{
+/**
+ * @brief Required time between measurements
+ * 
+ * @see BME280Init
+ */
+static uint32_t period = 0;
+
+/**
+ * @brief Device definition
+ * 
+ * @see BME280Init
+ */
+static struct bme280_dev dev;
+
+/**
+ * @brief Device settings
+ * 
+ * @see BME280Init
+ */
+static struct bme280_settings settings;
+
+
+BME280Status BME280Init(uint16_t addr) {
   int8_t rslt;
-  uint32_t period;
-  struct bme280_dev dev;
-  struct bme280_settings settings;
 
   /* Interface selection is to be updated as parameter
    * For I2C :  BME280_I2C_INTF
@@ -13,13 +31,22 @@ BME280Status BME280Init(uint16_t addr)
    */
   rslt = bme280_interface_selection(&dev, BME280_I2C_INTF);
   bme280_error_codes_print_result("bme280_interface_selection", rslt);
+  if (rslt != BME280_OK) {
+    return rslt;
+  }
 
   rslt = bme280_init(&dev);
   bme280_error_codes_print_result("bme280_init", rslt);
+  if (rslt != BME280_OK) {
+    return rslt;
+  }
 
   /* Always read the current settings before writing, especially when all the configuration is not modified */
   rslt = bme280_get_sensor_settings(&settings, &dev);
   bme280_error_codes_print_result("bme280_get_sensor_settings", rslt);
+  if (rslt != BME280_OK) {
+    return rslt;
+  }
 
   /* Configuring the over-sampling rate, filter coefficient and standby time */
   /* Overwrite the desired settings */
@@ -35,48 +62,60 @@ BME280Status BME280Init(uint16_t addr)
 
   rslt = bme280_set_sensor_settings(BME280_SEL_ALL_SETTINGS, &settings, &dev);
   bme280_error_codes_print_result("bme280_set_sensor_settings", rslt);
+  if (rslt != BME280_OK) {
+    return rslt;
+  }
 
   /* Always set the power mode after setting the configuration */
   rslt = bme280_set_sensor_mode(BME280_POWERMODE_NORMAL, &dev);
   bme280_error_codes_print_result("bme280_set_power_mode", rslt);
+  if (rslt != BME280_OK) {
+    return rslt;
+  }
 
   /* Calculate measurement time in microseconds */
   rslt = bme280_cal_meas_delay(&period, &settings);
   bme280_error_codes_print_result("bme280_cal_meas_delay", rslt);
+  if (rslt != BME280_OK) {
+    return rslt;
+  }
 
   printf("\nTemperature calculation (Data displayed are compensated values)\n");
   printf("Measurement time : %lu us\n\n", (long unsigned int)period);
 }
 
-BME280Status BME280MeasureTemperature(void)
-{
+BME280Status BME280MeasureAll(BME280Data *data) {
   int8_t rslt = BME280_E_NULL_PTR;
   uint8_t status_reg;
-  struct bme280_data comp_data;
 
-  rslt = bme280_get_regs(BME280_REG_STATUS, &status_reg, 1, dev);
+  rslt = bme280_get_regs(BME280_REG_STATUS, &status_reg, 1, &dev);
   bme280_error_codes_print_result("bme280_get_regs", rslt);
+  if (rslt != BME280_OK) {
+    return rslt;
+  }
 
   if (status_reg & BME280_STATUS_MEAS_DONE)
   {
     /* Measurement time delay given to read sample */
-    dev->delay_us(period, dev->intf_ptr);
+    dev.delay_us(period, dev.intf_ptr);
 
     /* Read compensated data */
-    rslt = bme280_get_sensor_data(BME280_TEMP, &comp_data, dev);
+    rslt = bme280_get_sensor_data(BME280_TEMP, data, &dev);
     bme280_error_codes_print_result("bme280_get_sensor_data", rslt);
-
-#ifndef BME280_DOUBLE_ENABLE
-    comp_data.temperature = comp_data.temperature / 100;
-#endif
-
-#ifdef BME280_DOUBLE_ENABLE
-    printf("Temperature[%d]:   %lf deg C\n", idx, comp_data.temperature);
-#else
-    printf("Temperature[%d]:   %ld deg C\n", idx, (long int)comp_data.temperature);
-#endif
-    idx++;
+    if (rslt != BME280_OK) {
+      return rslt;
+    }
   }
+
+  // adjust based on defines
+#ifndef BME280_DOUBLE_ENABLE
+  data->temperature = data->temperature / 100;
+  data->humidity = data->humidity / 1000;
+#endif
+    
+#ifdef BME280_64BIT_ENABLE 
+  data->pressure = data->pressure / 100;
+#endif
 
   return rslt;
 }

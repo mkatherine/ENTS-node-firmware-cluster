b3cb85bd9669d389837921464c104dc6338bdd59
John Madden <jmadden173@pm.me>
2024-08-23 10:42:36 -0700

    Outlined ModuleWiFi class

---
+++ esp32/lib/module_handler/include/modules/wifi.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file wifi.hpp
+ * @author John Madden (jmadden173@pm.me)
+ * @brief Module for WiFi communication
+ * @version 0.1
+ * @date 2024-08-23
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef ESP32_LIB_MODULE_HANDLER_INCLUDE_MODULES_WIFI_H_
+#define ESP32_LIB_MODULE_HANDLER_INCLUDE_MODULES_WIFI_H_
+
+#include "template_module.hpp"
+
+#include "soil_power_sensor.pb.h"
+
+class ModuleWiFi: public ModuleHandler::Module {
+  public:
+
+  ModuleWiFi(void);
+
+  ~ModuleWiFi(void);
+};
+
+#endif // ESP32_LIB_MODULE_HANDLER_INCLUDE_MODULES_WIFI_H_

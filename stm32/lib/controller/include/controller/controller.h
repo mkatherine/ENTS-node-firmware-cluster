5104d1d3612cb23f474938d63ded18811cd74c24
John Madden <jmadden173@pm.me>
2024-09-16 13:30:40 -0700

    Implemented controller init/deinit

--- stm32/lib/controller/include/controller/controller.h
+++ stm32/lib/controller/include/controller/controller.h
@@ -5,8 +5,18 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Initialize the esp32 controller
+ * 
+ * Currently nothing is done as the i2c peripherial is expected to already be
+ * initialized.In the future a quick communication check will happen.
+ */
 void ControllerInit(void);
 
+/**
+ * @brief Deinitialize the esp32 controller
+ * 
+ */
 void ControllerDeinit(void);
 
 #ifdef __cplusplus
--- stm32/lib/controller/src/controller.c
+++ stm32/lib/controller/src/controller.c
@@ -1,1 +1,7 @@
 #include "controller/controller.h"
+
+void ControllerInit(void) {
+  // TODO Add check for communication
+}
+
+void ControllerDeinit(void) {}
